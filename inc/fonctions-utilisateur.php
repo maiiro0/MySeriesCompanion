27670ef79c03f48cb894f0cd4f3d6f0ff21e853b
Elena Longuet <elenalonguet7@gmail.com>
2026-09-14 16:32:47 +0200

    connexion en cours

---
+++ inc/fonctions-utilisateur.php
@@ -0,0 +1,22 @@
+<?php 
+
+function inscription(PDO $pdo, $nom, $prenom, $email): void
+{
+    $requete = $pdo->prepare("INSERT INTO personne (email, nom, prenom) VALUES (:email, :nom, :prenom)");
+    $requete->bindParam('email', $email);
+    $requete->bindParam(':nom', $nom);
+    $requete->bindParam(':prenom', $prenom);
+
+    $requete->execute();
+}
+
+function utilisateurInfos($pdo, $nom, $prenom, $email): array
+{
+    $requete = $pdo->prepare("SELECT * FROM personne WHERE nom = :nom AND prenom = :prenom AND email = :email");
+    $requete->bindParam(':nom', $nom);
+    $requete->bindParam(':prenom', $prenom);
+    $requete->bindParam(':email', $email);
+    $requete->execute();
+    $user = $requete->fetch(PDO::FETCH_ASSOC);
+    return $user ?: null;
+}
